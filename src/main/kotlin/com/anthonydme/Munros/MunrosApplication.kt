294f15f070037d5b3237f9e662627374732e5808
Anthony Edwards <anthonydmedwards@gmail.com>
2021-09-11 20:12:48 +0100

    Initial commit

---
+++ src/main/kotlin/com/anthonydme/Munros/MunrosApplication.kt
@@ -0,0 +1,11 @@
+package com.anthonydme.Munros
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MunrosApplication
+
+fun main(args: Array<String>) {
+	runApplication<MunrosApplication>(*args)
+}
---
+++ src/test/kotlin/com/anthonydme/Munros/MunrosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anthonydme.Munros
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MunrosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
